5af5abd5320f0431b92df6476cde85613e413745
Dmytro <dimores95@gmail.com>
2022-12-09 13:32:52 +0200

    Dynamic components & import

--- app.vue
+++ app.vue
@@ -1,4 +1,25 @@
 <template>
-  <home-welcome />
-  <Counter />
+  <a href="#" @click="view('home')">Home</a>
+  <a href="#" @click="view('counter')">Counter</a>
+
+  <!-- <button @click="toggle">Toggle</button> -->
+  <LazyHomeWelcome v-if="page === 'home'" />
+
+  <!-- <component :is="MyComponent" /> -->
+  <LazyCounter v-if="page === 'counter'" />
 </template>
+
+<script setup>
+// const MyComponent = shallowRef(resolveComponent("Counter"));
+const MyComponent = ref("counter");
+
+function toggle() {
+  MyComponent.value = "welcome";
+}
+
+const page = ref("home");
+
+function view(name) {
+  page.value = name;
+}
+</script>
